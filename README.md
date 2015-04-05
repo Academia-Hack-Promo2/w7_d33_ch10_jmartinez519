1f2e487b0b0bd5e5ebe25436292671c0f87d9bc1
Jonathan Martinez <jamm@jmartinezm.org>
2015-04-05 10:59:36 -0430

    Creacion y actualizacion de user en API

---
+++ README.md
@@ -0,0 +1,1 @@
+# Reto todo cambio
---
+++ todo_cambio.rb
@@ -0,0 +1,55 @@
+# Utilizar el reto de HTTParty, el servicio cambió, ahora tiene dominio: hackapi.jgab.net
+
+# La data que existía en el servicio desapareció y el servicio sufrió modificaciones.
+
+# Registren su usuario de nuevo en el api.
+
+# Único cambio conocido del api: no se usa idenity, 
+# se filtra por username y debe ser el de slack.
+
+# NOTA: lean los errores que da el server.!!!, ahí estarán 
+# las respuestas que necesitan.
+
+# el resultado del reto saldrá por http://hackapi.jgab.net/api/users/hackers, 
+# para que puedan realizar el siguiente reto HTTP al menos 8 tienen que haber culminado el reto.
+
+
+require 'httparty'
+
+class Reto
+  include HTTParty
+  base_uri 'http://45.55.172.121:3000/api/users'
+
+  def agregar(user)
+    options = {body: user}
+    self.class.post('', options)
+  end
+
+  def listar
+    self.class.get('')
+  end
+
+  def editar(username, user)
+    options = {body: user}
+    self.class.put('/'+username, options)
+  end
+
+  def desactivar(username)
+    self.class.delete('/'+username)
+  end
+end
+
+jamm = Reto.new
+
+
+# print jamm.agregar({
+#   "address": "Los Dos Caminos", 
+#   "username": "@codejamm", 
+#   "fullname": "Jonathan Martinez", 
+#   "email": "jamm@hack4geeks.co",
+#   "gender": "male",
+#   });
+
+# print jamm.editar("@codejamm",{  
+#   "status": "active"
+#   });
